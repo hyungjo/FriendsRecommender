9b2234feeb5b89af2041921b389662cf7d6db433
서형조 <myexpnfut@gmail.com>
2016-05-06 00:41:33 +0900

    [OK] 레이아웃 재구성

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/error', function(req, res, next) {
+  res.render(error);
 });
 
 module.exports = router;
